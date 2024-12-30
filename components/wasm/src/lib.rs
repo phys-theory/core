0802de25e8a824c436e9da09cd135284fcdb0986
exidna <dm.a.titov@ya.ru>
2024-12-30 19:23:46 +0300

    feat: init

---
+++ components/wasm/src/lib.rs
@@ -0,0 +1,14 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    utils::set_panic_hook();
+    alert(&format!("Hello, {}!", name));
+}
